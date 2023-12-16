1eb55235d0b3f15df8e608ea6174fabefe361c88
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-12-16 09:11:39 +0100

    Complete task 2

--- modul_2_lekcja_4/Program.cs
+++ modul_2_lekcja_4/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // Task 1
             Employee employee = new()
             {
                 firstname = "John",
@@ -15,6 +16,18 @@
             };
 
             Console.WriteLine($"{employee.firstname} {employee.lastname}\nAge: {employee.age}\nPESEL: {employee.peselnumber}\nEmployee number: {employee.employeenumber}\nSex: {employee.sex}");
+
+            // Task 2
+            char a = 'a';
+            char b = 'b';
+            char c = 'c';
+
+            char[] letters = [a, b, c];
+
+            foreach( char letter in letters.Reverse())
+            {
+                Console.WriteLine(letter);
+            }
         }
     }
 }
