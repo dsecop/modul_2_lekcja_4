1527468f64ed70496cc76684b8f41e9c1c6984a4
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-12-16 08:25:27 +0100

    Add project files.

---
+++ modul_2_lekcja_4/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace modul_2_lekcja_4
+{
+    public class Employee
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+        public string peselnumber;
+        public long employeenumber;
+        public char sex;
+    }
+}
---
+++ modul_2_lekcja_4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace modul_2_lekcja_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new()
+            {
+                firstname = "John",
+                lastname = "Smith",
+                age = 23,
+                peselnumber = "73052622038",
+                employeenumber = 2509324094,
+                sex = 'm'
+            };
+
+            Console.WriteLine($"{employee.firstname} {employee.lastname}\nAge: {employee.age}\nPESEL: {employee.peselnumber}\nEmployee number: {employee.employeenumber}\nSex: {employee.sex}");
+        }
+    }
+}
